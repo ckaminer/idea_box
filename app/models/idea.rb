2b3c01c99906bd1673b064f05556931acd460d0b
Charles Kaminer <ckamine@g.clemson.edu>
2016-08-16 15:07:46 -0600

    implement idea deletion functionality added destroy route added destroy controller action tested controller action updated table rows to have delete button added ajax call to delete upon click added containers to views

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,6 +2,6 @@ class Idea < ApplicationRecord
   validates :title,   presence: true
   validates :body,    presence: true
   validates :quality, presence: true
-  
+
   enum quality: %w(swill plausible genius)
 end
